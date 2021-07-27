4d0d55916b8196a9d35baf4a74aaa625c9974e13
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-27 20:20:16 -0300

    feat: add basic functions

--- src/controller/BaseController.ts
+++ src/controller/BaseController.ts
@@ -14,6 +14,8 @@ import UIComponent from "sap/ui/core/UIComponent";
 import Model from "sap/ui/model/Model";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import View from "sap/ui/core/mvc/View";
+//import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import Router from "sap/ui/core/routing/Router";
 
 /**
  * Global fragments' ID list.
@@ -50,5 +52,60 @@ export default class BaseController extends Controller {
    */
   public setModel(model: JSONModel, modelName: string): View {
     return this.getView().setModel(model, modelName);
+  }
+
+  /**
+   * Convenience method for getting the resource bundle.
+   * @public
+   * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+   */
+  // public getResourceBundle(): ResourceModel {
+  //   return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+  // }
+
+  /**
+   * Convenience method for accessing the router in every controller of the application.
+   * @public
+   * @returns {sap.ui.core.routing.Router} the router for this component
+   */
+  public getRouter(): Router {
+    return UIComponent.getRouterFor(this);
+  }
+
+  /**
+   * Method for navigation to specific view
+   * @public
+   * @param {string} psTarget Parameter containing the string for the target navigation
+   * @param {mapping} pmParameters? Parameters for navigation
+   * @param {boolean} pbReplace? Defines if the hash should be replaced (no browser history entry) or set (browser history entry)
+   */
+  public navTo(
+    psTarget: string,
+    pmParameters?: object,
+    oComponentTargetInfo?: object,
+    pbReplace?: boolean
+  ): void {
+    this.getRouter().navTo(
+      psTarget,
+      pmParameters,
+      oComponentTargetInfo,
+      pbReplace
+    );
+  }
+
+  /**
+   * Event handler for navigating back.
+   * It there is a history entry or an previous app-to-app navigation we go one step back in the browser history
+   * If not, it will replace the current entry of the browser history with the master route.
+   * @public
+   */
+  public onNavBack(): void {
+    var sPreviousHash = History.getInstance().getPreviousHash();
+
+    if (sPreviousHash !== undefined) {
+      window.history.back();
+    } else {
+      this.getRouter().navTo("appHome", {}, {}, true /*no history*/);
+    }
   }
 }
