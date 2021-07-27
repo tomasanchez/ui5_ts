6bbdfd6eb7198f6df604e9e74097aa5df95f6648
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-27 20:22:04 -0300

    feat: inheritance of Base Controller

--- src/controller/App.controller.ts
+++ src/controller/App.controller.ts
@@ -1,18 +1,19 @@
 import MessageBox from "sap/m/MessageBox";
-import Controller from "sap/ui/core/mvc/Controller";
 import AppComponent from "../Component";
+import BaseController from "./BaseController";
 
 /**
  * @namespace com.ts.tsReport.controller
  */
-export default class AppController extends Controller {
+export default class AppController extends BaseController {
+  public onInit(): void {
+    // apply content density mode to root view
+    this.getView().addStyleClass(
+      (this.getOwnerComponent() as AppComponent).getContentDensityClass()
+    );
+  }
 
-	public onInit() : void {
-		// apply content density mode to root view
-		this.getView().addStyleClass((this.getOwnerComponent() as AppComponent).getContentDensityClass());
-	}
-
-	public sayHello() : void {
-		MessageBox.show("Hello World!");
-	}
+  public sayHello(): void {
+    MessageBox.show("Hello World!");
+  }
 }
