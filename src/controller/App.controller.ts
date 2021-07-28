38eea54a6597fec4324fae23951c22cf2a334a56
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-27 23:11:10 -0300

    refactor: frame app

--- src/controller/App.controller.ts
+++ src/controller/App.controller.ts
@@ -1,4 +1,14 @@
-import MessageBox from "sap/m/MessageBox";
+/**
+ * App Controller.
+ *
+ * App View controller container.
+ *
+ * @file This files describes App View controller.
+ * @author Tomas A. Sanchez
+ * @since 07.27.2021
+ */
+
+import JSONModel from "sap/ui/model/json/JSONModel";
 import AppComponent from "../Component";
 import BaseController from "./BaseController";
 
@@ -7,13 +17,32 @@ import BaseController from "./BaseController";
  */
 export default class AppController extends BaseController {
   public onInit(): void {
+    var oViewModel: JSONModel = new JSONModel({
+        busy: true,
+        delay: 0,
+      }),
+      iOriginalBusyDelay: int = this.getView().getBusyIndicatorDelay();
+
+    this.setModel(oViewModel, "appView");
+
+    var fnSetAppNotBusy: () => void = () => {
+      oViewModel.setProperty("/busy", false);
+      oViewModel.setProperty("/delay", iOriginalBusyDelay);
+    };
+
+    // disable busy indication when the metadata is loaded and in case of errors
+    var oModel: any = this.getOwnerComponent().getModel();
+
+    if (oModel) {
+      oModel.metadataLoaded().then(fnSetAppNotBusy);
+      oModel.attachMetadataFailed(fnSetAppNotBusy);
+    } else {
+      fnSetAppNotBusy();
+    }
+
     // apply content density mode to root view
     this.getView().addStyleClass(
       (this.getOwnerComponent() as AppComponent).getContentDensityClass()
     );
-  }
-
-  public sayHello(): void {
-    MessageBox.show("Hello World!");
   }
 }
